aab4aad4dacc76990683cccf43c3ea9fa9076a4e
rohithsowmithra <rohithsowmithra@gmail.com>
2020-11-18 12:31:17 +0530

    included database initial seeding

---
+++ manage.py
@@ -0,0 +1,36 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from app import APP
+from models import db, Movie, Actor
+
+migrate = Migrate(APP, db)
+manager = Manager(APP)
+
+@manager.command
+def seed():
+    """Creating initial few records in our database"""
+
+    actor1 = Actor(name = 'Actor-1', age = '35', gender = 'Male')
+    actor2 = Actor(name = 'Actor-2', age = '40', gender = 'Feale')
+    actor3 = Actor(name = 'Actor-3', age = '45', gender = 'Male')
+
+    actor1.insert()
+    actor2.insert()
+    actor3.insert()
+
+    movie1 = Movie(title='Movie-1', release_date='01-01-1970')
+    movie2 = Movie(title='Movie-2', release_date='01-01-1975')
+    movie3 = Movie(title='Movie-3', release_date='01-01-1980')
+
+    movie1.actors = [actor1, actor2]
+    movie2.actors = [actor2, actor3]
+    movie3.actors = [actor3, actor1]
+
+    movie1.insert()
+    movie2.insert()
+    movie3.insert()
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
