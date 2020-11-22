35e63d96e1fdf52c76541dae621a15a11bacc59e
rohithsowmithra <rohithsowmithra@gmail.com>
2020-11-22 16:34:19 +0530

    added route for fetching all movies

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, Movie, Actor, db
+from auth import requires_auth
+import json
 
 def create_app(test_config=None):
   # create and configure the app
@@ -11,11 +13,12 @@ def create_app(test_config=None):
 
   app.config['SECRET_KEY'] = 'dev'
 
-  CORS(app)
+  CORS(app, resources={r"/.*": {"origins": "*"}})
 
   return app
 
 app = create_app()
+
 
 @app.after_request
 def after_request(response):
@@ -27,7 +30,29 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  auth_server_details = {
+    'auth_host': os.environ.get('AUTH0_DOMAIN'),
+    'audience': os.environ.get('API_AUDIENCE'),
+    'client_id': os.environ.get('CLIENT_ID'),
+    'redirect_url': os.environ.get('AUTH0_REDIRECT_URL')
+  }
+  print("auth server details are: "+json.dumps(auth_server_details))
+  return render_template('index.html', auth_server_details = auth_server_details)
+
+
+@app.route('/movies')
+@requires_auth('view:movies')
+def get_all_movies():
+  try:
+    all_movies = Movie.query.all()
+    movies = [movie.format() for movie in all_movies]
+    return jsonify({
+      'success': True,
+      'movies': movies
+      })
+  except Exception:
+    abort(422)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
