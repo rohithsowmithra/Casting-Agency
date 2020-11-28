ec1e416dd2b5b971f6ee0731f83f79795a44e157
rohithsowmithra <rohithsowmithra@gmail.com>
2020-11-28 16:06:30 +0530

    updated all routes and error handlers

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, Movie, Actor, db
-from auth import requires_auth
+from auth import requires_auth, AuthError
 import json
+import sys
 
 
 def create_app(test_config=None):
@@ -53,7 +54,8 @@ def get_all_movies(payload):
       'movies': movies
       }), 200
   except Exception:
-    abort(422)
+      print(sys.exc_info())
+      abort(422)
 
 
 @app.route('/movies', methods = ['POST'])
@@ -74,6 +76,7 @@ def add_movie(payload):
         movie = Movie(title=title, release_date = release_date)
         movie.insert()
     except Exception:
+        print(sys.exc_info())
         abort(422)
     else:
         return jsonify({
@@ -95,12 +98,7 @@ def update_movie(payload, movie_id):
 
     title = body['title']
     release_date = body['release_date']
-
-    print(movie_id)
-
     movie = Movie.query.filter(Movie.id == movie_id).first()
-
-    print(movie)
 
     try:
         if not movie:
@@ -115,6 +113,7 @@ def update_movie(payload, movie_id):
         movie.update()
 
     except Exception:
+        print(sys.exc_info())
         movie.rollback()
         abort(422)
     else:
@@ -136,12 +135,163 @@ def delete_movie(payload, movie_id):
 
         movie.delete()
     except Exception:
+        print(sys.exc_info())
         abort(422)
     else:
         return jsonify({
             'success': True,
             'movie': movie_id
-        })
+        }), 200
+
+
+@app.route('/actors', methods = ['GET'])
+@requires_auth('view:actors')
+def get_all_actors(payload):
+    try:
+        actors = Actor.query.all()
+        return jsonify({
+            'success': True,
+            'actors': [actor.format() for actor in actors]
+        }), 200
+    except Exception:
+        print(sys.exc_info())
+        abort(422)
+
+
+@app.route('/actors', methods = ['POST'])
+@requires_auth('add:actors')
+def add_actor(payload):
+    body = request.get_json()
+
+    if not body:
+        abort(400)
+
+    if not ('name' in body and 'age' in body and 'gender' in body):
+        abort(400)
+
+    name = body['name']
+    age = body['age']
+    gender = body['gender']
+
+    try:
+        actor = Actor(name = name, age = age, gender = gender)
+        actor.insert()
+    except Exception:
+        print(sys.exc_info())
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'actor': name
+        }), 201
+
+
+@app.route('/actors/<int:actor_id>', methods = ['PATCH'])
+@requires_auth('update:actors')
+def update_actor(payload, actor_id):
+    body = request.get_json()
+
+    if not body:
+        abort(400)
+
+    if not ('name' in body or 'age' in body or 'gender' in body):
+        abort(400)
+
+    name = body['name']
+    age = body['age']
+    gender = body['gender']
+
+    actor = Actor.query.filter(Actor.id == actor_id).first()
+
+    try:
+        if not actor:
+            abort(404)
+
+        if name:
+            actor.name = name
+        if age:
+            actor.age = age
+        if gender:
+            actor.gender = gender
+
+        actor.update()
+
+    except Exception:
+        print(sys.exc_info())
+        actor.rollback()
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'actor': actor.format()
+        }), 200
+
+
+@app.route('/actors/<int:actor_id>', methods = ['DELETE'])
+@requires_auth('delete:actors')
+def delete_actor(payload, actor_id):
+    actor = Actor.query.filter(Actor.id == actor_id).first()
+
+    try:
+        if not actor:
+            abort(404)
+        actor.delete()
+    except Exception:
+        print(sys.exc_info())
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'actor': actor_id
+        }), 200
+
+
+# Error Handlers
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'bad request'
+    }), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found'
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'unprocessable entity'
+    }), 422
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'successs': False,
+        'code': 500,
+        'message': 'internal server error'
+    }), 500
+
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        'success': False,
+        'code': error.status_code,
+        'message': error.error
+    }), error.status_code
 
 
 if __name__ == '__main__':
