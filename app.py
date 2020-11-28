4c6d58d1aa7407916a461f5c9d687b8f5515df39
rohithsowmithra <rohithsowmithra@gmail.com>
2020-11-28 13:16:31 +0530

    added a few apis

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models import setup_db, Movie, Actor, db
 from auth import requires_auth
 import json
 
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -28,6 +29,7 @@ def after_request(response):
 
   return response
 
+
 @app.route('/')
 def index():
   auth_server_details = {
@@ -40,18 +42,106 @@ def index():
   return render_template('index.html', auth_server_details = auth_server_details)
 
 
-@app.route('/movies')
+@app.route('/movies', methods = ['GET'])
 @requires_auth('view:movies')
-def get_all_movies():
+def get_all_movies(payload):
   try:
     all_movies = Movie.query.all()
     movies = [movie.format() for movie in all_movies]
     return jsonify({
       'success': True,
       'movies': movies
-      })
+      }), 200
   except Exception:
     abort(422)
+
+
+@app.route('/movies', methods = ['POST'])
+@requires_auth('add:movies')
+def add_movie(payload):
+    body = request.get_json()
+
+    if not body:
+        abort(400)
+
+    if not ('title' in body and 'release_date' in body):
+        abort(400)
+
+    title = body['title']
+    release_date = body['release_date']
+
+    try:
+        movie = Movie(title=title, release_date = release_date)
+        movie.insert()
+    except Exception:
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'movie': title
+            }), 201
+
+
+@app.route('/movies/<int:movie_id>', methods = ['PATCH'])
+@requires_auth('update:movies')
+def update_movie(payload, movie_id):
+    body = request.get_json()
+
+    if not body:
+        abort(400)
+
+    if not ('title' in body or 'release_date' in body):
+        abort(400)
+
+    title = body['title']
+    release_date = body['release_date']
+
+    print(movie_id)
+
+    movie = Movie.query.filter(Movie.id == movie_id).first()
+
+    print(movie)
+
+    try:
+        if not movie:
+            abort(404)
+
+        if title:
+            movie.title = title
+
+        if release_date:
+            movie.release_date = release_date
+
+        movie.update()
+
+    except Exception:
+        movie.rollback()
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'movie': movie.format()
+        }), 200
+
+
+@app.route('/movies/<int:movie_id>', methods = ['DELETE'])
+@requires_auth('delete:movies')
+def delete_movie(payload, movie_id):
+
+    try:
+        movie = Movie.query.filter(Movie.id == movie_id).first()
+
+        if not movie:
+            abort(404)
+
+        movie.delete()
+    except Exception:
+        abort(422)
+    else:
+        return jsonify({
+            'success': True,
+            'movie': movie_id
+        })
 
 
 if __name__ == '__main__':
