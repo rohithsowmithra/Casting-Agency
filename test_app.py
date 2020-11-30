5049b3b571cfb58f659a135b0d891b575b2d54e9
rohithsowmithra <rohithsowmithra@gmail.com>
2020-11-30 18:58:34 +0530

    updated database url

--- test_app.py
+++ test_app.py
@@ -26,15 +26,9 @@ class CastAgencyTestCase(unittest.TestCase):
     """Setup test suite for the routes"""
     def setUp(self):
         """Setup application and database"""
-        self.db_test_user = 'postgres'
-        self.db_password = 'root'
-        self.database_name = 'cast_agency'
-        self.database_path = "postgresql://{}:{}@{}/{}".format(
-            self.db_test_user,
-            self.db_password,
-            'localhost:5432',
-            self.database_name
-        )
+
+        # self.database_path = os.environ.get('DATABASE_URL_LOCAL')
+        self.database_path = os.environ.get('DATABASE_URL')
 
         app.config['TESTING'] = True
         app.config['DEBUG'] = False
@@ -56,6 +50,7 @@ class CastAgencyTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(resp_data['success'], True)
         self.assertTrue(resp_data['movies'])
+        # self.assertTrue(resp_data['movies'][0]['actors'])
 
     # test to check if unauthorized user can access movies endpoint
     def test_401_to_access_movies_unauthorized_user(self):
@@ -69,8 +64,13 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check if producer can create a movie
     def test_201_when_producer_adds_movie(self):
         data = {
-            'title': 'Test-Movie-999',
-            'release_date': '2020-01-01'
+            'title': 'Test-Movie-111',
+            'release_date': '01-01-2002',
+            'actors': ['Actor-1',
+                       'Actor-2',
+                       'Actor-3',
+                       'Actor-X'
+                       ]
         }
         response = self.client.post('/movies',
                                     json=data,
@@ -85,8 +85,13 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that director can't create a movie
     def test_401_when_director_adds_movie(self):
         data = {
-            'title': 'Test-Movie-100',
-            'release_date': '01-01-2000'
+            'title': 'Test-Movie-222',
+            'release_date': '01-01-2000',
+            'actors': ['Actor-1',
+                       'Actor-2',
+                       'Actor-3',
+                       'Actor-X'
+                       ]
         }
         response = self.client.post('/movies',
                                     json=data,
@@ -100,8 +105,13 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that assistant can't create a movie
     def test_401_when_assistant_adds_movie(self):
         data = {
-            'title': 'Test-Movie-101',
-            'release_date': '01-01-2000'
+            'title': 'Test-Movie-333',
+            'release_date': '01-01-2000',
+            'actors': ['Actor-1',
+                       'Actor-2',
+                       'Actor-3',
+                       'Actor-X'
+                       ]
         }
         response = self.client.post('/movies',
                                     json=data,
@@ -115,7 +125,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that movie won't be created with bad args
     def test_400_when_adding_movie_with_invalid_args(self):
         data = {
-            'title': 'Test-Movie99'
+            'turtle': 'Test-Movie-99'
         }
         response = self.client.post('/movies',
                                     json=data,
@@ -130,8 +140,12 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that producer can edit a movie
     def test_200_when_producer_edits_movie(self):
         data = {
-            "title": "Test-Movie333",
-            "release_date": "01-01-2001"
+            'title': 'Test-Movie-444',
+            'release_date': '01-01-2001',
+            'actors': ['Actor-1',
+                       'Actor-2',
+                       'Actor-X'
+                       ]
         }
         movie_id = Movie.query.first().id
         response = self.client.patch(f'/movies/{movie_id}',
@@ -146,8 +160,12 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to checkthat director can edit a movie
     def test_200_when_director_edits_movie(self):
         data = {
-            "title": "Test-Movie444",
-            "release_date": "01-01-2002"
+            "title": "Test-Movie-555",
+            "release_date": "01-01-2002",
+            'actors': ['Actor-1',
+                       'Actor-3',
+                       'Actor-X'
+                       ]
         }
         movie_id = Movie.query.first().id
         response = self.client.patch(f'/movies/{movie_id}',
@@ -162,8 +180,12 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that assistant can't edit a movie
     def test_401_when_assistant_edits_movie(self):
         data = {
-            "title": "Test-Movie",
-            "release_date": "01-01-2003"
+            'title': 'Test-Movie-666',
+            'release_date': '01-01-2003',
+            'actors': ['Actor-2',
+                       'Actor-3',
+                       'Actor-X'
+                       ]
         }
         movie_id = Movie.query.first().id
         response = self.client.patch(f'/movies/{movie_id}',
@@ -178,8 +200,8 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that movie can't be updated with invalid args
     def test_422_edit_movie_with_invalid_args(self):
         data = {
-            "title": "",
-            "release_date": ""
+            'title': '',
+            'release_date': ''
         }
         movie_id = Movie.query.first().id
         response = self.client.patch(f'/movies/{movie_id}',
@@ -194,8 +216,11 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that producer can delete a movie
     def test_200_when_producer_deletes_movie(self):
         data = {
-            "title": "Test-Movie-999",
-            "release_date": ""
+            'title': 'Test-Movie-777',
+            'release_date': '01-01-1990',
+            'actors': ['Actor-1',
+                       'Actor-X'
+                       ]
         }
         self.client.post('/movies',
                          json=data,
@@ -253,6 +278,7 @@ class CastAgencyTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(resp_data['success'], True)
         self.assertTrue(resp_data['actors'])
+        # self.assertTrue(resp_data['actors'][0]['movies'])
 
     # test to check that unauthorized users can't access actors endpoint
     def test_401_to_access_actors_unauthorized_user(self):
@@ -266,7 +292,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that producer can create an actor
     def test_201_producer_adding_actor(self):
         data = {
-            'name': 'Test-Actor-499',
+            'name': 'Test-Actor-111',
             'age': 35,
             'gender': 'Male'
         }
@@ -283,7 +309,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that director can add an actor
     def test_201_when_director_adds_actor(self):
         data = {
-            'name': 'Test-Actor-500',
+            'name': 'Test-Actor-222',
             'age': 30,
             'gender': 'Male'
         }
@@ -300,7 +326,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that assistant can't create an actor
     def test_401_when_assistant_adds_actor(self):
         data = {
-            'name': 'Actor101',
+            'name': 'Test-Actor-333',
             'age': 25,
             'gender': 'Male'
         }
@@ -316,7 +342,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that actor cam't be created with bad args
     def test_400_when_adding_actor_with_invalid_args(self):
         data = {
-            'name': 'Actor-102'
+            'mane': 'Test-Actor-444'
         }
         response = self.client.post('/actors',
                                     json=data,
@@ -331,7 +357,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to chedk that prodcuer can edit an actor
     def test_200_when_producer_edits_actor(self):
         data = {
-            'name': 'Test-Actor-1',
+            'name': 'Test-Actor-555',
             'age': 30,
             'gender': 'Male'
         }
@@ -348,7 +374,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that director can edit an actor
     def test_200_when_director_edits_actor(self):
         data = {
-            'name': 'Test-Actor-2',
+            'name': 'Test-Actor-666',
             'age': 40,
             'gender': 'FeMale'
         }
@@ -365,7 +391,7 @@ class CastAgencyTestCase(unittest.TestCase):
     # test to check that assistant can't edit actor's details
     def test_401_when_assistant_edits_actor(self):
         data = {
-            'name': 'Test-Actor-3',
+            'name': 'Test-Actor-777',
             'age': 45,
             'gender': 'FeMale'
         }
